76d92b43d8c6b170c1701f1de11fa764c494a190
David Ackerman <david.s.ackerman@gmail.com>
2014-07-07 14:38:51 -0600

    Initial library commit for pushing to and fetching from a swift bucket.

---
+++ lmc/swift.py
@@ -0,0 +1,33 @@
+import pyrax, os, swiftclient
+
+def connection():
+  pyrax.settings.set('identity_type', 'keystone')
+  pyrax.set_setting("auth_endpoint", os.environ["OS_AUTH_URL"])
+  pyrax.set_credentials(username=os.environ["OS_USERNAME"], api_key=os.environ["OS_PASSWORD"], tenant_id=os.environ["OS_TENANT_ID"])
+  conn = pyrax.connect_to_cloudfiles(os.environ["OS_REGION_NAME"])
+  conn.max_file_size = 1073741823  # 1GB - 1
+  return conn
+
+def fetch(bucket, name, to_folder):
+  swift_connection = connection()
+  cont = swift_connection.get_container(bucket)
+  obj = cont.get_object(name)
+  fetch_path = os.path.join(to_folder, name)
+  file = open(fetch_path, "w")
+  for chunk in obj.fetch(chunk_size=1024*1024):
+    file.write(chunk)
+  file.close()
+
+def cache_fetch(bucket, name, cache_folder):
+  cache_path = os.path.join(cache_folder, name)
+  # TODO: also fetch if checksums don't match (don't just assume the file's good)
+  if not os.path.isfile(cache_path):
+    fetch(bucket=bucket, name=name, to_folder=cache_folder)
+
+def upload(bucket, name, from_folder):
+  swift_connection = connection()
+  cont = swift_connection.get_container(bucket)
+  from_path = os.path.join(from_folder, name)
+  # TODO: Only update if checksums don't match
+  cont.upload_file(from_path, name)
+
