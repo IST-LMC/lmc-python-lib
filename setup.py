938be6196214cf3ab549498d5dc6c0be55ae1626
David Ackerman <david.s.ackerman@gmail.com>
2015-07-28 14:03:59 -0600

    Add swift and keystone requirements.

--- setup.py
+++ setup.py
@@ -2,4 +2,7 @@ from setuptools import setup, find_packages
 setup(name='lmc-python-lib',
       version='1.0',
       packages=find_packages(),
+	  install_requires = [
+	  	'python-swiftclient',
+		'python-keystoneclient']
       )
