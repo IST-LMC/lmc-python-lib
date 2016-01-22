59e6c8f5513545d113914923952333490dd6759a
David Ackerman <david.s.ackerman@gmail.com>
2016-01-22 15:05:08 -0700

    Install libpq-dev dependency.

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -6,6 +6,7 @@ sudo apt-get install python-pip -y
 sudo apt-get install python-dev -y
 sudo apt-get install libffi-dev -y
 sudo apt-get install libssl-dev -y
+sudo apt-get install libpq-dev -y
 sudo pip install virtualenv
 
 # helpful tools (not really necessary for the python library)
