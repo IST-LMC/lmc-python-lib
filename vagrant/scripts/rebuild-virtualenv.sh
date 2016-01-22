0ef13404d012519bd78cae77b8fcfbeda7365c3f
David Ackerman <david.s.ackerman@gmail.com>
2016-01-22 15:32:12 -0700

    Nope... it's actually needed. How embarassing.

--- vagrant/scripts/rebuild-virtualenv.sh
+++ vagrant/scripts/rebuild-virtualenv.sh
@@ -5,6 +5,10 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     exit
 fi
 
+# Necessary for psycopg2 installation (lmc-packages does not run install.sh
+# when doing a package build)
+sudo apt-get install libpq-dev -y
+
 rm -fr /usr/local/lmc-python
 virtualenv -p /usr/bin/python2.7 /usr/local/lmc-python
 source /usr/local/lmc-python/bin/activate
