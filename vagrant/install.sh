37dc62e0c6f9d6d6580928744fadfb3928250594
David Ackerman <david.s.ackerman@gmail.com>
2016-05-10 16:18:36 -0600

    Fix broken virtualenv install

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -7,6 +7,7 @@ sudo apt-get install python-dev -y
 sudo apt-get install libffi-dev -y
 sudo apt-get install libssl-dev -y
 sudo apt-get install libpq-dev -y
+sudo apt-get install language-pack-en-base
 sudo pip install virtualenv
 
 # helpful tools (not really necessary for the python library)
